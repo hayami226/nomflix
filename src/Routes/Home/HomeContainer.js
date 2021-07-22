53273ca3d82925dd51e21da00b025cdcb38e3c01
Haram Park <hayami226@naver.com>
2021-07-22 21:44:14 +0900

    Added HomeContainer

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi } from "api";
 import React from "react";
 import HomePresenter from "./HomePresenter";
 
@@ -12,6 +13,33 @@ export default class extends React.Component{
         error: null,
         loading: true
     };
+
+    async componentDidMount() {
+        try {
+            const {
+                data: { results: nowPlaying }
+            } = await moviesApi.nowPlaying();
+            const {
+                data: { results: upcoming }
+            } = await moviesApi.upcoming();
+            const {
+                data: { results: popular }
+            } = await moviesApi.popular();
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular
+            });
+        } catch {
+            this.setState({
+                error: "Can't find movies information."
+            });
+        } finally {
+            this.setState({
+                loading: false
+            });
+        }
+    }
 
     render() {
         const { nowPlaying, upcoming, popular, error, loading } = this.state;
